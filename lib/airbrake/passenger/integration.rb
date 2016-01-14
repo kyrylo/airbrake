8f5a2970a335247a4c57cc986f494822bdd08492
Kyrylo Silin <silin@kyrylo.org>
2016-01-14 16:59:09 +0200

    +

--- lib/airbrake/passenger/integration.rb
+++ lib/airbrake/passenger/integration.rb
@@ -1,59 +1,24 @@
-module Airbrake
-  # Integration for Phusion Passenger
-  module Passenger
-    IntegrationError = Class.new(::StandardError)
-    class << self
-      def notifier
-        @notifier || :default
-      end
+PhusionPassenger.require_passenger_lib('loader_shared_helpers')
 
-      def notifier=(val)
-        if val.is_a?(Symbol)
-          @notifier = val
-        else
-          @notifier = val.to_sym
-        end
-      end
-    end
-    # Implementation of Passenger Integration for Versions 4.0 thru 5.0
-    module Integration
-      VERSION_REQUIREMENT = Gem::Requirement.new('>= 4.0', '< 5.1').freeze
-      class << self
-        def integrate!
-          return @integrated if defined?(@integrated)
-          raise IntegrationError, "Your version of PhusionPassenger " \
-            "is not currently supported by the airbrake gem." unless compatible?
-          unless defined?(::PhusionPassenger::LoaderSharedHelpers)
-            ::PhusionPassenger.require_passenger_lib 'loader_shared_helpers'
-          end
-          ::PhusionPassenger::LoaderSharedHelpers.send :extend, ClassMethods
-        rescue IntegrationError => e
-          Kernel.warn e
-          ::Airbrake.notify_sync(e, {}, notifier)
-          @integrated = false
-        else
-          @integrated = true
-        end
+module PhusionPassenger
+  module LoaderSharedHelpers
+    alias_method :about_to_abort_without_airbrake, :about_to_abort
 
-        def compatible?
-          @compatible ||= VERSION_REQUIREMENT.satisfied_by?(passenger_version)
-        end
+    def about_to_abort(*args)
+      puts 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+      puts args
+      p args
+      exception = args.last
 
-        private
+      return unless exception.is_a?(Exception)
 
-        def passenger_version
-          @passenger_version ||= begin
-            unless defined?(::PhusionPassenger)
-              raise IntegrationError, "PhusionPassenger not detected."
-            end
-            Gem::Version.new(::PhusionPassenger::VERSION_STRING)
-          end
-        end
-      end
+      params = {
+        component: "PhusionPassenger/#{PhusionPassenger::VERSION_STRING}"
+      }
+      Airbrake.notify_sync(exception, params, :passenger)
 
-      autoload :ClassMethods, 'airbrake/passenger/integration/class_methods'
+    ensure
+      about_to_abort_without_notifier(*args)
     end
   end
 end
-
-Airbrake::Passenger::Integration.integrate!
